8c898754f03c7417ac2b4a178547524a13800b1e
Mg Myat Htay Khant <mgmyathtaykhant@gmail.com>
2024-02-06 04:33:22 +0630

    Finished Project

---
+++ src/components/Form.jsx
@@ -0,0 +1,85 @@
+import { useEffect, useReducer, useRef } from "react";
+
+export default function Form() {
+
+    const [state, dispatch] = useReducer((state, action) => ({
+        ...state,
+        ...action
+    }), {
+        top: "",
+        bottom: "",
+        randomImage: "",
+        hasMemes: false,
+        isLoading: false,
+        allMemes: []
+    });
+    const firstRender = useRef(true);
+
+    useEffect(() => {
+        async function callingApi() {
+            try {
+                const response = await fetch("https://api.imgflip.com/get_memes");
+                const data = await response.json();
+                setTimeout(() => {
+                    let allMemes = data.data.memes;
+                    dispatch({ randomImage: getUrl(allMemes), allMemes, isLoading: false });
+                }, 3000);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        firstRender.current ? (firstRender.current = false) : callingApi();
+    }, [state.hasMemes]);
+
+
+    function getUrl(allMemes) {
+        const randomNumber = Math.floor(Math.random() * allMemes.length);
+        return allMemes[randomNumber].url;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        state.hasMemes ?
+            dispatch({ randomImage: getUrl(state.allMemes), top: "", bottom: "" }) :
+            dispatch({ hasMemes: true, isLoading: true });
+    }
+
+
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit} className="form">
+                <input
+                    type="text"
+                    className="top-text"
+                    placeholder="Top Text"
+                    value={state.top}
+                    onChange={(e) => dispatch({ top: e.target.value })}
+                />
+                <input
+                    type="text"
+                    className="bottom-text"
+                    placeholder="Bottom Text"
+                    value={state.bottom}
+                    onChange={(e) => dispatch({ bottom: e.target.value })}
+                />
+                <button disabled={state.isLoading} className="get-img-btn">
+                    Get a new meme image 🖼
+                </button>
+            </form>
+            {
+                state.hasMemes &&
+                    state.isLoading ? (
+                    <div className="loader"></div>
+                ) : (
+                    state.randomImage &&
+                    <div className="form-display">
+                        <img src={state.randomImage} alt="" />
+                        <h2 className="top-text">{state.top}</h2>
+                        <h2 className="bottom-text">{state.bottom}</h2>
+                    </div>
+                )
+            }
+        </div>
+    );
+}
